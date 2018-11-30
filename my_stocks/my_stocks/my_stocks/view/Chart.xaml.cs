0dd4a22ac7325b8307bb8e3aae4810fe46d21679
João Ferreira <noobcsf@hotmail.com>
2018-11-30 16:56:19 +0000

    Started Drawingh

--- my_stocks/my_stocks/my_stocks/view/Chart.xaml.cs
+++ my_stocks/my_stocks/my_stocks/view/Chart.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,30 @@ namespace my_stocks.view
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Chart: ContentPage
 	{
+        SKPaint fillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Black
+        };
+
 		public Chart ()
 		{
 			InitializeComponent ();
 		}
+
+        private void canvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            int width = e.Info.Width;
+            int height = e.Info.Height;
+
+            canvas.Clear(SKColors.CornflowerBlue);
+
+            canvas.DrawCircle(width/2f, height/2f , 100, fillPaint);
+
+            
+        }
 	}
 }
